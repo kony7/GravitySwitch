a2d4c073160916828976dc7210c35846d731509c
kony <kony7>
2020-09-28 02:24:28 +0900

    item material

---
+++ Assets/Scripts/JudgScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class JudgScript : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject highScoreText;
+    public int score = 0;
+    public int highScore;
+    public bool gameOverBool;
+
+    GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            if(score > highScore)
+            {
+                highScore = score;
+            }
+            Debug.Log("wall当たった");
+
+            highScoreText.GetComponent<Text>().text = "HighScore: " + score.ToString();
+
+            playerObject.GetComponent<PlayerScript>().DestroyMe();
+
+            gameOverCanvas.SetActive(true);
+            gameOverBool = true;
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+
+        if (other.gameObject.tag == "Item")
+        {
+            Debug.Log("item当たった");
+            score++;
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour
+{
+
+    private bool gameOverBool;
+    GameObject judgCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOverBool = GameObject.Find("JudgCllider");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if ()
+        //{
+
+        //}
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
